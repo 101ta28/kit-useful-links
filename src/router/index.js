94efc46ea78522799b6359bb69541609455b038f
101ta28 <workta28@gmail.com>
2023-04-02 21:04:04 +0900

    change router

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/',
+    path: '/kit-useful-links/',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
